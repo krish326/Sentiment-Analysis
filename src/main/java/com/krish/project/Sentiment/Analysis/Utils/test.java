a5a8b8fb6d61b160646b648f89ba4491b2c98e11
Krish Sharma <krishsharma@Krishs-MacBook-Air.local>
2025-06-05 08:08:10 +0530

    not using test as of now

--- src/main/java/com/krish/project/Sentiment/Analysis/Utils/test.java
+++ src/main/java/com/krish/project/Sentiment/Analysis/Utils/test.java
@@ -1,13 +1,8 @@
 package com.krish.project.Sentiment.Analysis.Utils;
 
-import com.krish.project.Sentiment.Analysis.Model.ReviewScraper;
 import com.krish.project.Sentiment.Analysis.Model.SentimentAnalyzer;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 
 public class test {
     public static void main(String[] args) throws IOException {
